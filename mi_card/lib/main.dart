0e4e8aaddc3bc6cb0644671f02833371b54e9202
Kabialnp4 <kabilanperumal6@gmail.com>
2022-08-05 11:15:14 +0530

    first commit

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -13,75 +13,78 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
-              child: Column(children: <Widget>[
-            CircleAvatar(
-              backgroundImage: AssetImage('images/me.jpeg'),
-              radius: 50.0,
-            ),
-            Text(
-              'Kabilan P',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                fontSize: 20.0,
-                color: Colors.teal.shade200,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.5,
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(children: <Widget>[
-                Icon(
-                  Icons.phone,
-                  color: Colors.teal,
-                ),
-                SizedBox(
-                  width: 10.0,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/me.jpeg'),
+                  radius: 50.0,
                 ),
                 Text(
-                  '+91 6379363225',
+                  'Kabilan P',
                   style: TextStyle(
-                    color: Colors.teal.shade900,
-                    fontFamily: 'Source Sans Pro',
-                    fontSize: 20.0,
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
-                ),
-              ]),
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(children: <Widget>[
-                Icon(
-                  Icons.mail,
-                  color: Colors.teal,
-                ),
-                SizedBox(
-                  width: 10.0,
                 ),
                 Text(
-                  'kabilanperumal777@gmail.com',
+                  'FLUTTER DEVELOPER',
                   style: TextStyle(
-                    color: Colors.teal.shade900,
                     fontFamily: 'Source Sans Pro',
                     fontSize: 20.0,
+                    color: Colors.teal.shade200,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
                   ),
-                )
-              ]),
-            ),
-          ]))),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+91 6379363225',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'kabilanperumal777@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ]))),
     );
   }
 }
